7e5cbdc545c0d317229ee811633c08a862104c43
owias azizi <owaisazizi360@gmail.com>
2025-02-17 20:09:39 +0430

    update text in project

--- src/Data/project.js
+++ src/Data/project.js
@@ -7,17 +7,18 @@ import pizza from '../assets/pizza.png'
 
 const projectArray=[
     {
-        image:weather,
-        title:'Weather App',
-        description:'Developed a responsive weather app using React.js, providing real-time weather updates for all cities in the world with an intuitive and user-friendly interface.',
-        techstack:["React","CSS"],
-        dLink:"https://weather-app-delta-ebon.vercel.app/",
-        gLink:"https://github.com/MOwaisAzizi/Weather-App"
+        image:pizza,
+        title:'Pizza-Shop',
+        description:'Developed a user-friendly interface for an online pizza shop, allowing you to buy your favorite pizza, put it in your basket as much as you want, and order it if you wish..',
+        techstack:["React","CSS","tailwind"],
+        dLink:"https://fast-react-pizza-v2.vercel.app/",
+        gLink:"https://github.com/MOwaisAzizi/fast-react-pizza-v2"
     },
+    
     {
         image:bank,
         title:'Bank app',
-        description:'Created an interactive and user-friendly bank application by HTML, CSS, and JavaScript, with ability to transform money from one account to another account(4 build in acounts), loan and withdraw money from your account.',
+        description:'Created an interactive and user-friendly bank application using HTML, CSS, and JavaScript, with the ability to transfer money from one account to another (4 built-in accounts), loan, and withdraw money from your account.',
         techstack:["HTML","CSS","Bootstrap","JavaScript"],
         dLink:"http://mowaisazizi.github.io/BankApplication/",
         gLink:"https://github.com/MOwaisAzizi/BankApplication"
@@ -31,11 +32,10 @@ const projectArray=[
         gLink:"https://github.com/MOwaisAzizi/library"
     },
 
-
     {
         image:shop,
         title:'Online Shope',
-        description:'Created an interactive and responsive online Shope using React.js, offering an efficient performance and ability to shope online. It feature seamless navigation, fast load times, and dynamic shope browsing and fake payment.',
+        description:'Created an interactive and responsive online shop using React.js, offering efficient performance and the ability to shop online. It features seamless navigation, fast load times, and dynamic shop browsing and fake payment.',
         techstack:["React","CSS",'JavaScript','BootStrap'],
         dLink:"http://online-market-phi.vercel.app",
         gLink:"https://github.com/MOwaisAzizi/Online-Market"
@@ -43,20 +43,21 @@ const projectArray=[
     {
         image:mapty,
         title:'Mapty',
-        description:'Developed a mapty website using JavaScrp, featuring a dynamic and user-friendly interface for showing different locations in the world and abilty to store your activity in locations and see it after comming later.',
+        description:'Developed a Mapty website using JavaScript, featuring a dynamic and user-friendly interface for showing different locations in the world and the ability to store your activities in locations and see them after coming back later',
         techstack:["HTML","CSS","JavaScript(OOP)"],
         dLink:"https://mapty-gules.vercel.app",
         gLink:"https://github.com/MOwaisAzizi/Mapty-ToDoList"
         
     },
     {
-        image:pizza,
-        title:'Pizza-Shop',
-        description:'Developed a user-friendly interface Pizza online shope, in order to buy your favorite piza, put it in your basket as much as you want and order it if you want.',
-        techstack:["React","CSS","tailwind"],
-        dLink:"https://fast-react-pizza-v2.vercel.app/",
-        gLink:"https://github.com/MOwaisAzizi/fast-react-pizza-v2"
-    }
+        image:weather,
+        title:'Weather App',
+        description:'Developed a responsive weather app using React.js, providing real-time weather updates for all cities in the world with an intuitive and user-friendly interface',
+        techstack:["React","CSS"],
+        dLink:"https://weather-app-delta-ebon.vercel.app/",
+        gLink:"https://github.com/MOwaisAzizi/Weather-App"
+    },
+
 ]
 
 export default projectArray;
