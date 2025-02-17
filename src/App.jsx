fd9821b892b105e7630042e5b3069716d3c51245
owias azizi <owaisazizi360@gmail.com>
2025-02-17 23:00:50 +0430

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <>
       <div className="fixed inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-      <main className='flex flex-col items-center px-2 md:px-4 lg:px-16' >
+      <main className='flex flex-col items-center px-2 sm:px-3 md:px-4 lg:px-16' >
         
         <Header/>
 
--- src/Componets/TechStack/TechStack.jsx
+++ src/Componets/TechStack/TechStack.jsx
@@ -21,7 +21,7 @@ function TechStack() {
             transition={{duration:0.5}}
             className='text-4xl text-white font-semibold mt-10 md:text-5xl md:mt-20'>Skills</motion.h1>
 
-            <div className='flex flex-wrap items-center justify-center gap-5 md:gap-6 p-2 pt-0'>
+            <div className='flex flex-wrap items-center justify-center gap-6 p-2 pt-0'>
 
                 <motion.div variants={variants} initial="hidden" whileInView="visible" transition={{duration:0.5}}>
                   <BiLogoHtml5 className='cursor-pointer text-[80px] text-red-500 transition-all duration-300 hover:translate-y-5 sm:text-[100px] md:text-[100px]' />
@@ -77,7 +77,7 @@ function TechStack() {
                 
 
                 <motion.div variants={variants} initial="hidden" whileInView="visible" transition={{duration:0.5}}>
-                  <BiLogoJava className='cursor-pointer text-[80px] text-blue-500 transition-all duration-300 hover:translate-y-5 sm:text-[100px] md:text-[100px]' />
+                  <BiLogoJava className='cursor-pointer text-[80px] text-blue-400 transition-all duration-300 hover:translate-y-5 sm:text-[100px] md:text-[100px]' />
                 </motion.div>
               
                  
