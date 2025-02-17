0a6cb1194e1bbfad9fb62e7959e14b24ca718e5d
owias azizi <owaisazizi360@gmail.com>
2025-02-17 20:53:22 +0430

    refectored

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Footer from './Componets/Footer/Footer'
 import Education from './Componets/Education/Education'
 
 export default function App() {
-
   return (
     <>
       <div className="fixed inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
