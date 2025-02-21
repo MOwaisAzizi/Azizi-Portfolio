0c5aeae8b6930893721ab6f5edee0ad21983f31d
owias azizi <owaisazizi360@gmail.com>
2025-02-22 00:37:28 +0430

    update

--- src/Componets/Education/Education.jsx
+++ src/Componets/Education/Education.jsx
@@ -8,7 +8,7 @@ function Education() {
 
   return (
     <div
-      className="text-white flex flex-col items-center justify-center pt-32 pb-28 px-7 sm:px-10 md:p-32 md:pb-32  "
+      className="text-white flex flex-col items-center justify-center pt-32 pb-28 px-7 sm:px-10 md:p-32 md:pb-32 scroll-mb-40"
       id="education"
     >
       <motion.h2
