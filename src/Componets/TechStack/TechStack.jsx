3be69fe9c501c0f9879172b54d939fa4745d4982
owias azizi <owaisazizi360@gmail.com>
2025-02-21 23:53:05 +0430

    update mini text

--- src/Componets/TechStack/TechStack.jsx
+++ src/Componets/TechStack/TechStack.jsx
@@ -1,8 +1,5 @@
 import { motion } from 'motion/react'
-import React from 'react'
 import {BiLogoHtml5,BiLogoGit,BiLogoGithub,BiLogoRedux,BiLogoCss3, BiLogoBootstrap,BiLogoTailwindCss, BiLogoJavascript, BiLogoReact, BiLogoNodejs,BiLogoJava,BiLogoMongodb } from 'react-icons/bi'
-import { GrMysql } from 'react-icons/gr'
-import { SiExpress } from 'react-icons/si'
 
 function TechStack() {
 
@@ -21,7 +18,7 @@ function TechStack() {
             transition={{duration:0.5}}
             className='text-4xl text-white font-semibold mt-16 md:text-5xl md:mt-24'>Skills</motion.h1>
 
-            <div className='flex flex-wrap items-center justify-center gap-6 p-2 pt-0'>
+            <div className='flex flex-wrap items-center justify-center gap-6 p-2 sm:px-3 pt-0'>
 
                 <motion.div variants={variants} initial="hidden" whileInView="visible" transition={{duration:0.5}}>
                   <BiLogoHtml5 className='cursor-pointer text-[80px] text-red-500 transition-all duration-300 hover:translate-y-5 sm:text-[100px] md:text-[100px]' />
