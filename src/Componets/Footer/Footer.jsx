bf7cee2c21c189fa27c0cc478e1a13eb92742978
owias azizi <owaisazizi360@gmail.com>
2025-03-26 23:48:05 +0430

    update style

--- src/Componets/Footer/Footer.jsx
+++ src/Componets/Footer/Footer.jsx
@@ -7,11 +7,11 @@ function Footer() {
     <footer className='bg-black/60 p-6 rounded-sm'>
       <div className='text-center mb-4 text-2xl text-white'>+93798267860</div>
        <div className="flex gap-8 items-center justify-center">
-        <a href="https://github.com/MOwaisAzizi" target='_blank'><BsGithub className='text-3xl text-white cursor-pointer  hover:text-blue-500' /></a>
-        <a href="https://www.linkedin.com/in/m-o-azizi" target='_blank'><BsLinkedin className='text-3xl text-blue-600 cursor-pointer   hover:text-blue-600' /></a>
-        <a href="https://flowcv.com/resume/ao3k3188fp" target='_blank'><FaFileAlt className='text-3xl text-white cursor-pointer  hover:text-blue-500' /></a>
-        <a href="https://t.me/EngOwais" target='_blank'><BsTelegram className='text-3xl cursor-pointer text-blue-500 hover:text-blue-500' /></a>
-        <a href="https://wa.me/93798267860" target='_blank'><BsWhatsapp className='text-3xl cursor-pointer text-green-400 hover:text-blue-500' /></a>
+        <a href="https://github.com/MOwaisAzizi" target='_blank'><BsGithub className='text-3xl cursor-pointer text-gray-300  hover:text-blue-500' /></a>
+        <a href="https://www.linkedin.com/in/m-o-azizi" target='_blank'><BsLinkedin className='text-3xl cursor-pointer text-gray-300  hover:text-blue-600' /></a>
+        <a href="https://flowcv.com/resume/ao3k3188fp" target='_blank'><FaFileAlt className='text-3xl cursor-pointer text-gray-300 hover:text-blue-500' /></a>
+        <a href="https://t.me/EngOwais" target='_blank'><BsTelegram className='text-3xl cursor-pointer text-gray-300 hover:text-blue-500' /></a>
+        <a href="https://wa.me/93798267860" target='_blank'><BsWhatsapp className='text-3xl cursor-pointer text-gray-300 hover:text-blue-500' /></a>
       </div>
 
       <div className="flex gap-4 sm:gap-6 items-center justify-center mt-5">
